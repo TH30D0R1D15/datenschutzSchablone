e631a4563f93ba5158a2a6f880449138293a7bb4
TH30D0R1D15 <paskal.theodoridis@hotmail.de>
2023-04-08 09:58:14 +0200

    Zeichnen geht jetzt mit gedrückter maustaste

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ imageUpload.addEventListener('change', handleImageUpload);
 
 // Event-Listener für Maskierungsmodus-Button
 const maskingModeButton = document.getElementById('masking-mode');
-maskingModeButton.addEventListener('click', toggleMaskingMode);
+maskingModeButton.addEventListener('mousedown', toggleMaskingMode);
 
 // Event-Listener für Bild-Maskieren-Button
 const maskImageButton = document.getElementById('mask-image');
@@ -40,52 +40,79 @@ function toggleMaskingMode() {
   if (maskingMode) {
     maskingModeButton.innerText = 'Maskierungsmodus beenden';
     // Event-Listener für Maus-Klicks im Canvas
-    canvas.addEventListener('click', handleMaskRegion);
+    canvas.addEventListener('mousedown', handleMaskRegion);
+    canvas.addEventListener('mousemove', handleMaskRegion);
   } else {
     maskingModeButton.innerText = 'Maskierungsmodus';
     // Event-Listener für Maus-Klicks im Canvas entfernen
-    canvas.removeEventListener('click', handleMaskRegion);
+    canvas.removeEventListener('mousedown', handleMaskRegion);
+    canvas.removeEventListener('mousemove', handleMaskRegion);
   }
 }
 
 // Markierten Bereich hinzufügen
 function handleMaskRegion(event) {
-  const x = event.offsetX;
-  const y = event.offsetY;
-  maskRegions.push({x: x, y: y});
-  // Markierung zeichnen
-  context.beginPath();
-  context.arc(x, y, 10, 0, Math.PI * 2);
-  context.strokeStyle = 'green';
-  context.stroke();
+  if (maskingMode && event.buttons === 1) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    maskRegions.push({x: x, y: y});
+    // Markierung zeichnen
+    context.beginPath();
+    context.arc(x, y, 10, 0, Math.PI * 2);
+    context.strokeStyle = 'green';
+    context.stroke();
+  }
 }
 
 // Bild maskieren
 function maskImage() {
-  // Schleife über alle Pixel im Canvas
-  const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-  for (let i = 0; i < imageData.data.length; i += 4) {
-    const x = (i / 4) % canvas.width;
-    const y = Math.floor((i / 4) / canvas.width);
-    // Prüfen, ob Pixel markiert ist
-    let masked = false;
-    for (let j = 0; j < maskRegions.length; j++) {
-      const region = maskRegions[j];
-      const dx = x - region.x;
-      const dy = y - region.y;
-      if (dx * dx + dy * dy < 100) {
-        masked = true;
-        break;
+    // Schleife über alle Pixel im Canvas
+    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < imageData.data.length; i += 4) {
+      const x = (i / 4) % canvas.width;
+      const y = Math.floor((i / 4) / canvas.width);
+      // Prüfen, ob Pixel markiert ist
+      let masked = false;
+      for (let j = 0; j < maskRegions.length; j++) {
+        const region = maskRegions[j];
+        const dx = x - region.x;
+        const dy = y - region.y;
+        if (dx * dx + dy * dy < 100) {
+          masked = true;
+          break;
+        }
+      }
+      // Pixel schwärzen, wenn nicht markiert
+      if (!masked) {
+        imageData.data[i] = 0;
+        imageData.data[i + 1] = 0;
+        imageData.data[i + 2] = 0;
+        imageData.data[i + 3] = 255;
       }
     }
-    // Pixel schwärzen, wenn nicht markiert
-    if (!masked) {
-      imageData.data[i] = 0;
-      imageData.data[i + 1] = 0;
-      imageData.data[i + 2] = 0;
-      imageData.data[i + 3] = 255;
-    }
+    // Maskiertes Bild in Canvas zeichnen
+    context.putImageData(imageData, 0, 0);
   }
-  // Maskiertes Bild in Canvas zeichnen
-  context.putImageData(imageData, 0, 0);
-}
+  
+  // Event-Listener für Maus-Down im Canvas
+  canvas.addEventListener('mousedown', function(event) {
+    if (maskingMode) {
+      isDrawing = true;
+      const x = event.offsetX;
+      const y = event.offsetY;
+      maskRegions.push({x: x, y: y});
+      // Markierung zeichnen
+      context.beginPath();
+      context.arc(x, y, 10, 0, Math.PI * 2);
+      context.strokeStyle = 'green';
+      context.stroke();
+    }
+  });
+  
+  // Event-Listener für Maus-Up im Canvas
+  canvas.addEventListener('mouseup', function(event) {
+    if (maskingMode) {
+      isDrawing = false;
+    }
+  });
+  
